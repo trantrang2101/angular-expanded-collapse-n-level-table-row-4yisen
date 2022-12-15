a709bbfd057450bb0d5437553fd1659d68b7d699
trantrang2101 <79267668+trantrang2101@users.noreply.github.com>
2022-12-15 07:12:33 +0700

    update dynamic nested json

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,11 @@
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {RecursiveComponent} from './recursive-component'
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-  declarations: [AppComponent,RecursiveComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
